1ed1307a41edaf50350b3995e75ecd1a16cf9a7f
Baixue Yao <snow.cedar.yao@gmail.com>
2023-02-28 08:58:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,17 +51,19 @@ def construct_graph(touching):
 def threshold_graph_edge_weight(G, threshold_value):
     for edge in G.edges:
         if G.edges[edge]['weight']<threshold_value:
-            G.remove_edge(edge[0],edge[1])
+            G.remove_edge(edge[0],edge[1])# remove edges whose weight is smaller than a threshold value
 
-    dict_cc = {}
-    for cc in list(nx.connected_components(G)):
+    dict_cc = {}# cc: connected component in the graph
+    # if one connected component is (1,2,5), add {2:1, 5:1} to dict_cc to indicate that label 2 is relabeled as 1 and label 5 is also relabeled as label 1
+    for cc in list(nx.connected_components(G)):# for each connected component
         if len(cc)==1:
-            continue
-        cc_min = min(cc)
-        cc_non_min = cc-set([cc_min])
+            continue# no merge needed if the cc only contains one label
+        cc_min = min(cc)# choose the min value of all labels in the cc as the final label after merge
+        cc_non_min = cc-set([cc_min])# all labels in the cc except for the chosen label
         for ele in cc_non_min:
             dict_cc[ele] = cc_min
 
+    # if label only have 6 labels np.array([0,1,2,3,4,5]), {2:1, 5:1} generates np.array([0,1,1,3,4,1])
     arr_cc = np.zeros(label_values[-1]+1,dtype=np.uint32)
     for label_value in label_values:
         if label_value in dict_cc.keys():
@@ -90,15 +92,18 @@ def saveMP4(MP4_path,arr3d,fps=20):
     writer.close()
     print('Saved '+MP4_path)
 
-class WatershedVisualize:
+class RandomColormap:
+    """
+    apply random colormap to a label image
+    """
     def __init__(self,num_features):
         np.random.seed(0)
         self.cmap = matplotlib.colors.ListedColormap(np.vstack([[[1,1,1],],np.random.rand(num_features-1,3)]))
         self.norm = matplotlib.colors.Normalize(vmin=0,vmax=num_features)
         self.scalar_mappable = matplotlib.cm.ScalarMappable(norm=self.norm, cmap=self.cmap)
-    def toRGB(self,arr):
+    def toRGB(self,arr):# accepts 2d gray scale image 
         return self.scalar_mappable.to_rgba(arr,bytes=True)[...,:3]
-    def toRGB_by_slice(self,arr3d):
+    def toRGB_by_slice(self,arr3d):# accepts 3d gray scale image
         assert hasattr(arr3d,'ndim') and arr3d.ndim==3, "only accept 3D array"
         return np.stack([self.toRGB(arr3d[z]) for z in np.arange(arr3d.shape[0])],0)
 
@@ -116,5 +121,5 @@ if __name__ == "__main__":
     particles_arr_thresh3000 = threshold_graph_edge_weight(G.copy(), 3000)[particles_arr]
 
     stacked = np.dstack([particles_arr,particles_arr_thresh3000])# stack horizontally
-    wsrgb = WatershedVisualize(stacked.max()).toRGB_by_slice(stacked)
+    wsrgb = RandomColormap(stacked.max()).toRGB_by_slice(stacked)
     saveMP4('ws_threshold_contact_surface_thresh3000.mp4',wsrgb)
